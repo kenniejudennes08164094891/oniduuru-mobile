3527de005711d18891fa97bb2f5f502e3412fe4a
Kehinde Omosehin <kehindejude1995@gmail.com>
2025-12-04 14:04:15 +0100

    fix:merging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-export const environment = {
+export const environment:any = {
   production: false,
   baseUrl: 'https://oniduuru-staging.shoftafrica.com',//'https://oniduuru-staging.shoftafrica.com', // http://localhost:7000
   oniduuruLandingPage: 'https://shoft-africa-apps-catalogue.vercel.app/landing-pages/oniduuru-marketplace',
-  useMockData: true,
 };
