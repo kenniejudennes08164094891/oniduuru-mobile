45972c22cc84b37dc246dea1ca445f32e015def7
Kehinde Omosehin <kehindejude1995@gmail.com>
2026-02-04 04:13:19 +0100

    fix: Cleanup for QA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://oniduuru-staging.shoftafrica.com', // 'http://localhost:7000', //
+  baseUrl: 'https://oniduuru-staging.shoftafrica.com', //'http://localhost:7000', //
    // Add Google Maps API key
   googleMapsApiKey: 'AIzaSyDwlkVSi56mNcDtPasXHj-viQIGs2DIN0c'
 };
